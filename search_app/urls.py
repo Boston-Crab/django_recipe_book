0df8af96fc106b0b205e7ad956b5459b795b4a1d
Matas <matassabaliauskas1@gmail.com>
2024-07-03 13:31:26 +0200

    feat: get_recipe_by_id view handling + front end page for specific recipe

--- search_app/urls.py
+++ search_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from search_app.views import get_recipies_by_name, index
+from search_app.views import get_recipies_by_name, index, get_recipe_by_id
 
 urlpatterns = [
     path('api/get-recipes/', get_recipies_by_name, name="get_recipes"),
-    path("", index)
+    path('api/get_recipe/<str:meal_id>/', get_recipe_by_id, name='get_recipe_by_id'),
+    path("", index),
 ]
--- search_app/views.py
+++ search_app/views.py
@@ -14,20 +14,32 @@ def get_recipies_by_name(request):
         if response.status_code == 200:
             data = response.json()
             if data['meals'] is not None:
-                for meal in data['meals']:
-                    for i in range(1, 21):
-                        ingredient_key = f'strIngredient{i}'
-                        measurement_key = f'strMeasure{i}'
-                        if ingredient_key in meal and meal[ingredient_key] == '':
-                            del meal[ingredient_key]
-                        if measurement_key in meal and meal[measurement_key] == '':
-                            del meal[measurement_key]
-                
-                return HttpResponse(data['meals'])
+                context = {'data': data['meals']}
+
+                return render(request, "search_app/search_results.html", context)
             else:
                 return render(request, "search_app/index.html", {'message': f"No meals found for '{search_term}'. Try something else."})
         else:
             return render(request, "search_app/index.html")
+        
+def get_recipe_by_id(request, meal_id):
+    print(type(meal_id))
+    url = f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={meal_id}'
+    response = requests.get(url)
+    if response.status_code == 200:
+            data = response.json()
+            if data['meals'] is not None:
+                for meal in data['meals']:
+                    for i in range(1, 21):
+                        ingredient_key = f'strIngredient{i}'
+                        measurement_key = f'strMeasure{i}'
+                        if ingredient_key in meal and (meal[ingredient_key] == '' or meal[ingredient_key] is None):
+                            del meal[ingredient_key]
+                        if measurement_key in meal and (meal[measurement_key] == '' or meal[measurement_key] is None):
+                            del meal[measurement_key]
+                context = {'data': data['meals']}
+
+                return render(request, 'search_app/recipe_details.html', context)
 
 
 def index(request):
