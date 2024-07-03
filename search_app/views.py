91c224d69ce6143d747c5cb21ebff9f56a3ac60b
Matas <matassabaliauskas1@gmail.com>
2024-07-03 11:43:09 +0200

    feat: no-such-item handling and front end representation

--- search_app/views.py
+++ search_app/views.py
@@ -11,19 +11,23 @@ def get_recipies_by_name(request):
         url = f"https://www.themealdb.com/api/json/v1/1/search.php?s={
             search_term}"
         response = requests.get(url)
-        data = response.json()
-
-        for meal in data['meals']:
-            for i in range(1, 21):
-                ingredient_key = f'strIngredient{i}'
-                measurement_key = f'strMeasure{i}'
-                if ingredient_key in meal and meal[ingredient_key] == '':
-                    del meal[ingredient_key]
-                if measurement_key in meal and meal[measurement_key] == '':
-                    del meal[measurement_key]
+        if response.status_code == 200:
+            data = response.json()
+            if data['meals'] is not None:
+                for meal in data['meals']:
+                    for i in range(1, 21):
+                        ingredient_key = f'strIngredient{i}'
+                        measurement_key = f'strMeasure{i}'
+                        if ingredient_key in meal and meal[ingredient_key] == '':
+                            del meal[ingredient_key]
+                        if measurement_key in meal and meal[measurement_key] == '':
+                            del meal[measurement_key]
                 
-        print(data)
-        return HttpResponse(data['meals'])
+                return HttpResponse(data['meals'])
+            else:
+                return render(request, "search_app/index.html", {'message': f"No meals found for '{search_term}'. Try something else."})
+        else:
+            return render(request, "search_app/index.html")
 
 
 def index(request):
